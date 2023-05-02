b56fef1eb79d4c3736b4c05ea97c9dcea2758741
Lorenzo Bevilacqua <43483037+ardubev16@users.noreply.github.com>
2023-05-02 23:20:58 +0200

    build(update): Update plugins

--- lua/plugins/ui/nvim-tree.lua
+++ lua/plugins/ui/nvim-tree.lua
@@ -7,7 +7,6 @@ local M = {
 }
 
 function M.config()
-    local tree_cb = require('nvim-tree.config').nvim_tree_callback
     local icons = require('user.settings').icons.diagnostics
 
     local function open_nvim_tree(data)
@@ -31,18 +30,31 @@ function M.config()
     end
     vim.api.nvim_create_autocmd({ 'VimEnter' }, { callback = open_nvim_tree })
 
+    local function on_attach(bufnr)
+        local api = require('nvim-tree.api')
+
+        local function opts(desc)
+            return {
+                desc = 'nvim-tree: ' .. desc,
+                buffer = bufnr,
+                noremap = true,
+                silent = true,
+                nowait = true,
+            }
+        end
+
+        api.config.mappings.default_on_attach(bufnr)
+
+        vim.keymap.set('n', 'l', api.node.open.edit, opts('Open'))
+        vim.keymap.set('n', '<CR>', api.node.open.edit, opts('Open'))
+        vim.keymap.set('n', 'o', api.node.open.edit, opts('Open'))
+        vim.keymap.set('n', 'h', api.node.navigate.parent_close, opts('Close Directory'))
+        vim.keymap.set('n', 'v', api.node.open.vertical, opts('Open: Vertical Split'))
+    end
+
     require('nvim-tree').setup({
         disable_netrw = true,
-        view = {
-            mappings = {
-                custom_only = false,
-                list = {
-                    { key = { 'l', '<CR>', 'o' }, cb = tree_cb('edit') },
-                    { key = 'h', cb = tree_cb('close_node') },
-                    { key = 'v', cb = tree_cb('vsplit') },
-                },
-            },
-        },
+        on_attach = on_attach,
         actions = {
             open_file = {
                 quit_on_open = true,
